d832a43f66517e0ce3e755007e687f3e30fbfbfe
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-01-10 19:16:18 -0700

    created env file

--- server/models/mongooseDb.js
+++ server/models/mongooseDb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dbUrl = process.env.MONGO_URL || 'mongodb+srv://daedalus:icarus@project3daedalus.nzx15.mongodb.net/Project3Daedalus?retryWrites=true&w=majority'  //|| 'mongodb://localhost:27017/c7Superheroes'
+const dbUrl = process.env.MONGO_URL 
 mongoose.connect(dbUrl)
 
 module.exports = mongoose

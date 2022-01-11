6cd08ffef921671f38c1b5cd649de0e67cdd49e5
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-01-10 19:24:24 -0700

    deleted env file

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
-const express = require('express')
+const express = require("express");
 
-const path = require('path')
-
+const path = require("path");
+require("dotenv").config();
 
 // const D2dRoutes = require('./routes/D2dRoutes')
 
-
-const app = express()
-const port = process.env.PORT || 5001
+const app = express();
+const port = process.env.PORT || 5001;
 
 // app.use('/api',D2dRoutes)//
-app.use('/', express.static('../client/build'))
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname,"../client/build","index.html"))
-  })
+app.use("/", express.static("../client/build"));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});

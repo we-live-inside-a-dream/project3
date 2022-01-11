ccc3127d62d90c27dc964b04bf6584e4355f7643
derek-birtwistle <derekbirtwistle@hotmail.com>
2022-01-10 17:56:40 -0700

    connected to mongo atlas

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+
+const path = require('path')
+
+
 // const D2dRoutes = require('./routes/D2dRoutes')
 
 
 const app = express()
-const port = 5001
+const port = process.env.PORT || 5001
 
 // app.use('/api',D2dRoutes)//
 app.use('/', express.static('../client/build'))
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname,"../client/build","index.html"))
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

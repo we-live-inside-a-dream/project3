ac6c6c58903ff9e12961f370b94d292220ed05ba
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-01-13 11:27:38 -0700

    created employee row

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import WeekSingleEmployee from './components/WeekSingleEmployee';
 
 function App() {
   return (
     <div className="App">
-      
+      <WeekSingleEmployee />
     </div>
   );
 }

08d2cdfbbcb9a5a49b7716dcf16465345b6cc1d6
derek-birtwistle <derekbirtwistle@hotmail.com>
2022-01-13 18:59:26 -0700

    env import

--- server/models/mongooseDb.js
+++ server/models/mongooseDb.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-const dbUrl = process.env.MONGO_URL 
+require("dotenv").config("../.env");
+
+
+const dbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/c7Superheroes'
+
 mongoose.connect(dbUrl)
 
 module.exports = mongoose

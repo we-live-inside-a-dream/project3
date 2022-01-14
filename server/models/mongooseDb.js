bfb6dbe5707851f8fbf2a77ae1abbca0fb9c5379
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-13 19:38:51 -0700

    Database connection modifications.

--- server/models/mongooseDb.js
+++ server/models/mongooseDb.js
@@ -5,6 +5,8 @@ require("dotenv").config("../.env");
 const dbUrl =
   process.env.MONGO_URL || "mongodb://localhost:27017/c7Superheroes";
 
+
+
 mongoose
   .connect(dbUrl, {
     useNewURLParser: true,

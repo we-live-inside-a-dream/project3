5508bfc2c178aee6f9095b9b0c136cfa0ce9482d
derek-birtwistle <derekbirtwistle@hotmail.com>
2022-01-14 18:46:55 -0700

    schedule edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import DynamicScheduleTrial from "./components/DynamicScheduleTrial";
+import EditSchedule from "./components/EditSchedule";
 
 function App() {
   return (
     <div className="App">
       <DynamicScheduleTrial />
+ 
+      <EditSchedule />
     </div>
   );
 }

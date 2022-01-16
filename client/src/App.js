2a5a164a95e6774cf901e5ea89bdd29bea0ba6b8
derek-birtwistle <derekbirtwistle@hotmail.com>
2022-01-15 17:17:26 -0700

    added modal component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import DynamicScheduleTrial from "./components/DynamicScheduleTrial";
 import EditSchedule from "./components/EditSchedule";
+import Modal from "./Modal";
+import {useState} from 'react'
+
 
 function App() {
+  const[isOpen,setIsOpen] = useState(false)
   return (
+    <>
     <div className="App">
       <DynamicScheduleTrial />
- 
-      <EditSchedule />
+     
+        
+        <button onClick={()=>setIsOpen(true)}> Schedule</button>
+        <Modal open={isOpen} onClose={()=>setIsOpen(false)}>
+        <EditSchedule onClose={onclose} />  
+          </Modal> 
+        
+        
+      
+       
+        
+      
     </div>
+    <div>
+ 
+    </div>
+    </>
   );
 }
 

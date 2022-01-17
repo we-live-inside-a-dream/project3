7809f482aa2b4ecd94cd3d500ea73251840aea77
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-17 11:21:26 -0700

    Commented out 'mustBeLoggedIn'

--- server/routes/employeeProfileRoutes.js
+++ server/routes/employeeProfileRoutes.js
@@ -10,22 +10,22 @@ const {
 } = require("../models/employeeProfile");
 
 
-const mustBeLoggedIn = async (req, res, next) => {
-  if (req.user) {
-    next();
-    return;
-  }
-  res.sendStatus(401);
-};
+// const mustBeLoggedIn = async (req, res, next) => {
+//   if (req.user) {
+//     next();
+//     return;
+//   }
+//   res.sendStatus(401);
+// };
 
-router.post("/create", mustBeLoggedIn, async (req, res) => {
+router.post("/create", async (req, res) => {
   let newEmployeeProfile = req.body;
   let employeeProfileId = await createEmployeeProfile(newEmployeeProfile)
   if (!profile) res.status(500).send("failed to create");
   res.status(200).send(profile);
 });
 
-router.put("/update", mustBeLoggedIn, async (req, res) => {
+router.put("/update", async (req, res) => {
   console.log(req.body)
   let updateEmployeeProfile = req.body;
   updateEmployeeProfile(employeeProfile, (updatedModel) => {
@@ -38,13 +38,13 @@ router.get('/employees', async (req, res) => {
   res.send(employeeList)
 })
 
-router.get("/getByEmail/:email", mustBeLoggedIn, async (req, res) => {
+router.get("/getByEmail/:email", async (req, res) => {
   let email = req.params.email;
   let employeeEmail = await findEmployeeByProfileEmail(email);
   res.status(200).send(profile);
 });
 
-router.get("/getByProfileId/:profileId", mustBeLoggedIn, async (req, res) => {
+router.get("/getByProfileId/:profileId", async (req, res) => {
   let profileId = req.params.profileId;
   let profile = await getEmployeeProfileByProfileId(profileId);
   res.status(200).send(profile);

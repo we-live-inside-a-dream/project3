f97badea7e5f73e888e7b63e3a8a4b89a065ce7f
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-17 20:05:30 -0700

    Added Availability Router.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const path = require("path");
 require("dotenv").config();
-const scheduleRoutes = require("./routes/scheduleRoutes");
-
-
+const scheduleRouter = require("./routes/scheduleRoutes");
+const availabilityRouter = require("./availabilityRoutes");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 // app.use(bodyParser.urlencoded({ extended: false, limit:'10mb' }));
 // app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/employeeProfile', employeeProfileRouter)
-// app.use('/availability', availabilityRouter)
+app.use("/employeeProfile", employeeProfileRouter);
+app.use("/availability", availabilityRouter);
 
-
-
-app.use("/api/schedule", scheduleRoutes); 
+app.use("/api/schedule", scheduleRouter);
 app.use("/", express.static("../client/build"));
 
 app.use("*", (req, res) => {

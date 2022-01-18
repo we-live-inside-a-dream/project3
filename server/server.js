ae7eedbf9c36a6df80a5702a343a1cebbeac45d5
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-18 09:43:24 -0700

    Fixed error routes.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const bodyParser = require("body-parser");
 const path = require("path");
-require("dotenv").config();
+
 const scheduleRouter = require("./routes/scheduleRoutes");
-// const availabilityRouter = require("./routes/availabilityRoutes");
-// const employeeProfileRouter = require("./routes/employeeRoutes")
+const availabilityRouter = require("./routes/availabilityRoutes");
+const employeeProfileRouter = require("./routes/employeeProfileRoutes")
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -13,8 +14,8 @@ const port = process.env.PORT || 5001;
 // app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-// app.use("/employeeProfile", employeeProfileRouter);
-// app.use("/availability", availabilityRouter);
+app.use("/employeeProfile", employeeProfileRouter);
+app.use("/availability", availabilityRouter);
 
 app.use("/api/schedule", scheduleRouter);
 app.use("/", express.static("../client/build"));
@@ -26,3 +27,4 @@ app.use("*", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+

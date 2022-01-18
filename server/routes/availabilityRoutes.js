2cde3e5b31b92a149c17a829a5c2e352ccc48113
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-17 20:07:25 -0700

    Edited Availability Routes.

--- server/routes/availabilityRoutes.js
+++ server/routes/availabilityRoutes.js
@@ -1,17 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
-    createAvailability,
-    updateAvailability,
-    getAvailabilityByEmployeeProfileId,
-    deleteAvailability,
-} = require("../models/availability")
+  createAvailability,
+  updateAvailability,
+} = require("../models/availability");
 
-router.post("/createAvailability", async (req, res) => {
-    let newAvailability = req.body;
-    let availability = await createAvailability(newAvailability)
-    if (!profile) res.status(500).send("failed to create");
-    res.status(200).send(profile);
+router.post("/availability", async (req, res) => {
+  let newAvailability = req.body;
+  let availability = await createAvailability(newAvailability);
+  if (!profile) res.status(500).send("failed to create");
+  res.status(200).send(availability);
+});
+
+router.patch("/availability/:id", async (req, res) => {
+  let id = req.params.id;
+  let updatedAvailability = req.body;
+  console.log("Updating availability", id, "with", updatedAvailability);
+  let availability = await availabilityModel.update(id, updatedAvailability);
+  updateAvailability(availability, (updatedModel) => {
+    res.status(200).send(updatedModel);
   });
+});
 
+router.get("/availability/:id", async (req, res) => {
+  let availabilityList = await availabilityModel.listAvailabilities();
+  res.send(availabilityList);
+});

1429dada89b8309012cc4656f3736d4926f535b6
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-19 10:11:24 -0700

    Commented out dotenv.config in mongooseDb.

--- server/models/mongooseDb.js
+++ server/models/mongooseDb.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require('path')
  
-dotenv.config({path:path.join(__dirname,'../.env')});
-console.log(__dirname)
+// dotenv.config({path:path.join(__dirname,'../.env')});
+// console.log(__dirname)
 const dbUrl = process.env.MONGO_URI;
 
 mongoose.connect(dbUrl, () => {

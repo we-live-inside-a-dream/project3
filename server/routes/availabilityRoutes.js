144626418a78a5d53f842d14e361115816d2e34d
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-01-19 12:44:29 -0700

    added employee availability table and added it to routes and react router

--- server/routes/availabilityRoutes.js
+++ server/routes/availabilityRoutes.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createAvailability,
-  updateAvailability,
-} = require("../models/availability");
+const availabilityModel = require("../models/availability");
 
-router.post("/availability", async (req, res) => {
-  let newAvailability = req.body;
-  let availability = await createAvailability(newAvailability);
-  if (!profile) res.status(500).send("failed to create");
-  res.status(200).send(availability);
-});
+// router.post("/availability", async (req, res) => {
+//   let newAvailability = req.body;
+//   let availability = await createAvailability(newAvailability);
+//   if (!profile) res.status(500).send("failed to create");
+//   res.status(200).send(availability);
+// });
 
-router.patch("/availability/:id", async (req, res) => {
-  let id = req.params.id;
-  let updatedAvailability = req.body;
-  console.log("Updating availability", id, "with", updatedAvailability);
-  let availability = await availabilityModel.update(id, updatedAvailability);
-  updateAvailability(availability, (updatedModel) => {
-    res.status(200).send(updatedModel);
-  });
-});
+// router.patch("/availability/:id", async (req, res) => {
+//   let id = req.params.id;
+//   let updatedAvailability = req.body;
+//   console.log("Updating availability", id, "with", updatedAvailability);
+//   let availability = await update(id, updatedAvailability);
+//   updateAvailability(availability, (updatedModel) => {
+//     res.status(200).send(updatedModel);
+//   });
+// });
 
-router.get("/availability/:id", async (req, res) => {
-  let availabilityList = await availabilityModel.listAvailabilities();
-  res.send(availabilityList);
-});
+// router.get("/availability/:id", async (req, res) => {
+//   let availabilityList = await listAvailabilities();
+//   res.send(availabilityList);
+// });
 router.get("/availability-all", async (req, res) => {
-  let availabilityList = await availabilityModel.listOfEmployees();
-  res.send(availabilityList);
+  console.log("from AvailabilityRoutes");
+  let availabilityList =
+    await availabilityModel.listOfEmployeesAvailabilities();
+  console.log("List from Availability routes", availabilityList);
+  res.json(availabilityList);
 });
 module.exports = router;

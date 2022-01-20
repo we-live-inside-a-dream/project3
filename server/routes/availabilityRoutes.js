0e883c009d97b7acbfd7faf4081a27cf4a70c1b9
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-01-19 17:55:15 -0700

    fixed routes for availability

--- server/routes/availabilityRoutes.js
+++ server/routes/availabilityRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const availabilityModel = require("../models/availability");
 
-// router.post("/availability", async (req, res) => {
+// router.post("/availability-add", async (req, res) => {
 //   let newAvailability = req.body;
 //   let availability = await createAvailability(newAvailability);
 //   if (!profile) res.status(500).send("failed to create");
@@ -24,43 +24,52 @@ const availabilityModel = require("../models/availability");
 //   let availabilityList = await listAvailabilities();
 //   res.send(availabilityList);
 // });
-// router.get("/availability-all", async (req, res) => {
+// router.get("/availability-all", mustBeLoggedIn, async (req, res) => {
 //   console.log("from AvailabilityRoutes");
 //   let availabilityList =
 //     await availabilityModel.listOfEmployeesAvailabilities();
 //   console.log("List from Availability routes", availabilityList);
 //   res.json(availabilityList);
-// const mustBeLoggedIn = async (req, res, next) => {
-//   if (req.user) {
-//     next();
-//     return;
-//   }
+//   const mustBeLoggedIn = async (req, res, next) => {
+//     if (req.user) {
+//       next();
+//       return;
+//     }
+//   };
 //   res.sendStatus(401);
-// };
+// });
 
-router.post("/availability", mustBeLoggedIn, async (req, res) => {
-  let newAvailability = req.body;
-  let availability = await createAvailability(newAvailability);
-  if (!profile) res.status(500).send("failed to create");
-  res.status(200).send(availability);
-});
+// router.post("/availability", mustBeLoggedIn, async (req, res) => {
+//   let newAvailability = req.body;
+//   let availability = await createAvailability(newAvailability);
+//   if (!profile) res.status(500).send("failed to create");
+//   res.status(200).send(availability);
+// });
 
-router.patch("/availability/:id", mustBeLoggedIn, async (req, res) => {
-  let id = req.params.id;
-  let updatedAvailability = req.body;
-  console.log("Updating availability", id, "with", updatedAvailability);
-  let availability = await availabilityModel.update(id, updatedAvailability);
-  updateAvailability(availability, (updatedModel) => {
-    res.status(200).send(updatedModel);
-  });
-});
+// router.patch("/availability/:id", mustBeLoggedIn, async (req, res) => {
+//   let id = req.params.id;
+//   let updatedAvailability = req.body;
+//   console.log("Updating availability", id, "with", updatedAvailability);
+//   let availability = await availabilityModel.update(id, updatedAvailability);
+//   updateAvailability(availability, (updatedModel) => {
+//     res.status(200).send(updatedModel);
+//   });
+// });
 
 // router.get("/availability/:id", mustBeLoggedIn, async (req, res) => {
 //   let availabilityList = await availabilityModel.listAvailabilities();
 //   res.send(availabilityList);
 // });
-router.get("/availability-all", mustBeLoggedIn, async (req, res) => {
-  let availabilityList = await availabilityModel.listOfEmployees();
+router.get("/availability-all", async (req, res) => {
+  let availabilityList =
+    await availabilityModel.listOfEmployeesAvailabilities();
   res.send(availabilityList);
+});
+
+router.post("/availability-add", async (req, res) => {
+  let newAvailability = req.body;
+  let availability = await createAvailability(newAvailability);
+  if (!profile) res.status(500).send("failed to create");
+  res.status(200).send(availability);
 });
 module.exports = router;

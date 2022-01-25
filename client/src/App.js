7608a22ad6ddc9dd16c3e215cb4b0c6effe2e35c
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-01-24 21:02:32 -0700

    added function to convert date object for weekSchedule

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import "./App.css";
 import CalendarComponent from "./components/UNUSED/CalendarComponent";
-import "./App.css";
-// import { useState } from "react";
-// import background from "./assets/boldPic.jpg";
 import DaySchedulePage from "./pages/common/DaySchedulePage";
 import EmployeesList from "./pages/manager/EmployeesList";
 import { Routes, Route } from "react-router-dom";
-
 import NavBar from "./components/navigation/NavBar";
 import EmployeeEditForm from "./components/employee-list/EmployeeEditForm";
 import EmployeeAvailabilityPage from "./pages/manager/EmployeeAvailabilityPage";
 import EmployeeAvailabilityEditPage from "./pages/manager/EmployeeAvailabilityEditPage";
 import AvailabilityDetailPage from "./pages/manager/AvailabilityDetailPage";
 import WeekSchedulePage from "./pages/common/WeekSchedulePage";
-import WeekSchedulePractise from "./components/week-schedule/WeekSchedulePractise";
+// import background from "./assets/boldPic.jpg";
 
 function App() {
   return (
@@ -42,7 +38,6 @@ function App() {
           path="/availability-detail/:id"
           element={<AvailabilityDetailPage />}
         />
-        <Route path="/theweek" element={<WeekSchedulePractise />} />
       </Routes>
     </div>
   );
--- client/src/components/week-schedule/WeekSchedule.js
+++ client/src/components/week-schedule/WeekSchedule.js
@@ -4,8 +4,6 @@ import StyledTableHeader from "../reusable/tables/StyledTableHeader";
 import moment from "moment";
 import StyledTable from "../reusable/tables/StyledTable";
 import Modal from "../reusable/Modal";
-import { eachDayOfInterval } from "date-fns";
-import { LeftContainer } from "../navigation/StyledNavBar";
 
 function WeekSchedule() {
   moment().format();
@@ -21,48 +19,23 @@ function WeekSchedule() {
   //   setStartDay();
   //   setEndDay("2022-02-01");
   // }, []);
-  console.log("startDay", startDay, "endDay", endDay);
+
   const findDateRange = function (startDay, endDay) {
-    let dates = [];
+    let datesArray = [];
 
     let firstDate = moment(startDay).startOf("day");
     firstDate.subtract(1, "days");
     let lastDate = moment(endDay).startOf("day");
 
     while (firstDate.add(1, "days").diff(lastDate) <= 0) {
-      console.log(firstDate.toDate());
-      dates.push(firstDate.clone().toDate());
+      // console.log(firstDate.toDate());
+      datesArray.push(firstDate.clone().format("dddd, Do").toString());
+      // formatDate(datesArray);
     }
     // console.log(dates);
-    return dates;
+    setWeek(datesArray);
+    console.log(week);
   };
-
-  // useEffect(() => {
-  //   console.log("shiftId is", shiftId);
-  //   const fetchShift = async () => {
-  //     let fetchResult = await fetch(`/api/schedule/id?id=${shiftId}`);
-  //     console.log("fetch result", fetchResult);
-  //     let fetchedShift = await fetchResult.json();
-  //     console.log("fetchedShift is", fetchedShift);
-  //     setShift(fetchedShift);
-  //     setIsOpen(true);
-  //   };
-  //   fetchShift();
-  // }, [shiftId]);
-
-  // useEffect(() => {
-  //   console.log("day is ", day);
-  //   const fetchSchedule = async () => {
-  //     let fetchResult = await fetch(`/api/schedule/day?day=${day}`);
-  //     console.log("fetch result", fetchResult);
-  //     let fetchedDay = await fetchResult.json();
-  //     console.log("fetchedDay is", fetchedDay);
-
-  //     setSchedule(fetchedDay);
-  //   };
-  //   fetchSchedule();
-  // }, [day]);
-
   // let startTime = 8;
   // let endTime = 18;
   // let businessHours = [];
@@ -111,7 +84,9 @@ function WeekSchedule() {
           id="single-day"
           name="day"
           value={endDay}
-          onChange={(e) => setEndDay(e.target.value)}
+          onChange={(e) => {
+            setEndDay(e.target.value);
+          }}
         />
       </h1>
       <p>{startDay}</p>
@@ -120,120 +95,17 @@ function WeekSchedule() {
         SHOW WEEK
       </button>
 
-      {/* <StyledTable>
+      <StyledTable>
         <thead>
           <tr>
             <th>NAME</th>
-            {headerHours?.map((hour) => {
-              if (hour === Math.floor(hour)) {
-                return (
-                  <StyledTableHeader style={{ width: "50px", padding: "0px" }}>
-                    {hour}
-                  </StyledTableHeader>
-                );
-              } else if (hour - 0.5 === Math.floor(hour)) {
-                return (
-                  <StyledTableHeader
-                    style={{
-                      width: "50px",
-                      padding: "0px",
-                    }}
-                  ></StyledTableHeader>
-                );
-              } else
-                return (
-                  <StyledTableHeader
-                    style={{
-                      width: "50px",
-                      padding: "0px",
-                    }}
-                  ></StyledTableHeader>
-                );
+            {week?.map((day, index) => {
+              return <StyledTableHeader index={index}>{day}</StyledTableHeader>;
             })}
           </tr>
         </thead>
-        {/* <tbody>
- {schedule?.map((employee, index) => {
-   return(
-     <ScheduleRow 
-   schedule={schedule}
-   buisnessHours={buisnessHours}
-     
-     />
-   )
- }
-
-        </tbody> */}
-      {/* <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          <EditSchedule
-            shiftId={shiftId}
-            existingValues={shift}
-            onClose={() => setIsOpen(false)}
-          />
-          **edit**
-        </Modal>
-        <tbody>
-          {schedule?.map((employee, index) => (
-            <tr key={index} onClick={() => setShiftId(employee._id)}>
-              <td key={index}>
-                <div style={{ display: "inline-flex" }}>
-                  <div
-                    style={{
-                      backgroundColor: "grey",
-                      height: "3rem",
-                      width: "2rem",
-                      marginRight: "10px",
-                      alignSelf: "center",
-                    }}
-                  ></div>
-                  <div
-                    style={{
-                      margin: "auto 10px auto 10px",
-                      color: "#4488AB",
-                      fontWeight: "600",
-                      display: "block",
-                    }}
-                  >
-                    <p>{employee.name}</p>
-                    <p
-                      style={{
-                        textShadow: "none",
-                        color: "#545454",
-                        fontSize: ".7rem",
-                      }}
-                    >
-                      {employee.start.slice(0, 2)}-{employee.end.slice(0, 2)}
-                    </p>
-                  </div>
-                </div>
-              </td>
-
-              {businessHours?.map((hour, index) => {
-                if (
-                  hour >= convertTime(employee.start) &&
-                  hour < convertTime(employee.end)
-                ) {
-                  return (
-                    <td key={index}>
-                      <div
-                        style={{
-                          backgroundColor: "#5AB9EA",
-                          height: "45px",
-                          padding: "0px",
-                          border: "1px solid #5AB9EA",
-                          margin: "25px 0",
-                        }}
-                      ></div>
-                    </td>
-                  );
-                } else {
-                  return <td key={index}></td>;
-                }
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </StyledTable>  */}
+        <tbody></tbody>
+      </StyledTable>
     </div>
   );
 }

6b5bae54c455223e0414ac3a5c24dd6faeb5c9db
derek-birtwistle <derekbirtwistle@hotmail.com>
2022-01-25 16:44:18 -0700

    employeeDetail has changes?

---
+++ client/src/pages/manager/CreateEmployeePage.js
@@ -0,0 +1,24 @@
+import EmployeeEditForm from "../../components/employee-list/EmployeeEditForm";
+
+import { useNavigate } from "react-router-dom";
+
+const CreateEmployeePage = () => {
+    // let navigate = useNavigate();
+    // async function createEmployee(newEmployee) {
+    //     // const newEmployee = {firstName: "", lastName: ""}
+    //     await fetch("/api/employeeProfile/create", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify(newEmployee),
+    //       // body: newEmployee
+    //     });
+    //     navigate("/employeeList");
+    //   }
+    return (
+        <EmployeeEditForm  />
+    )
+}
+
+export default CreateEmployeePage

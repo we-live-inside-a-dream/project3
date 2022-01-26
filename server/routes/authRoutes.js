c893055f0aaf455a8add9155e5286a3dc4f63501
Brian Sauco <brian.sauco.95@gmail.com>
2022-01-26 00:07:10 -0700

    Added JWT tokens for authentication.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
-
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
-const employeeProfile = require("../models/employeeProfile");
-
+const EmployeeProfile = require("../models/employeeProfile");
 const router = express.Router();
+const Token = require("../models/token")
 
 passport.use(
   new LocalStrategy(
@@ -13,67 +11,126 @@ passport.use(
       usernameField: "email",
       passwordField: "password",
     },
-    function (email, password, done) {
-      console.log("Passport is trying to verify a profile", email);
-      employeeProfile
+    function verify(email, password, done) {
+      EmployeeProfile
         .findEmployeeByProfileEmail(email)
-        .then((user) => {
-          if (!user || user.password !== password) {
+        .then((email) => {
+          if (!email) {
+            return done(null, false);
+          }
+          if (!email || employeeProfile.password !== password) {
             done(null, false, {
               message: "Email not found or incorrect password",
             });
             return;
           }
-          done(null, user);
+          done(null, username);
         })
         .catch(done);
     }
   )
 );
 
-passport.serializeUser(function (user, done) {
-  console.log("passport wants to store this user in a cookie", user);
-  done(null, user.id);
-});
 
-passport.deserializeUser(function (id, done) {
-  console.log("passport is trying to recover the user from the cookie", id);
-  User.findById(id)
-    .then((user) => {
-      if (!user) {
-        done(new Error("User not found or deleted"));
-        return;
-      }
-      done(null, user);
-    })
-    .catch(done);
-});
-
-// router.post("/signIn", async (req, res) => {
-//   console.log(req.user);
-//   let employeeProfileInfo = req.body.inputs;
-//   let user = await signIn(employeeProfileInfo);
-//   res.send(user);
+// passport.serializeUser(function (email, done) {
+//   console.log("passport wants to store this user in a cookie", email);
+//   done(null, email.id);
 // });
 
-router.post("/logIn", passport.authenticate("local"), async (req, res) => {
-  // If this function gets called, authentication was successful.
-  // `req.user` contains the authenticated user.
-  let employeeProfileInfo = req.body.inputs;
-  let user = await logIn(employeeProfileInfo);
-  console.log("User login has succeeded!");
-  console.log("Req.user is", req.user);
-  res.sendStatus(200);
-  res.send(user);
-});
+// passport.deserializeUser(function (id, done) {
+//   console.log("passport is trying to recover the user from the cookie", id);
+//   User.findById(id)
+//     .then((email) => {
+//       if (!email) {
+//         done(new Error("User not found or deleted"));
+//         return;
+//       }
+//       done(null, email);
+//     })
+//     .catch(done);
+// });
 
-router.get("/logOut", async (req, res) => {
-  req.logout();
-  res.sendStatus(200);
-});
+router.post("/login", (req, res) => {
+  EmployeeProfile.findOne({ email: req.body.email }).exec().then((employeeProfile) => {
+    if(!employeeProfile){
+      return res.status(401).json({
+        message: "User not found",
+        data: undefined
+      })
+    }
+    bcrypt.compare(req.body.pasword, employeeProfile.password, async (err, result) => {
+      if(err){
+        return res.status(401).json({
+          status: false,
+          message: "Server error, authentication failed",
+          data: undefined
+        })
+      }
+      if(result){
+        const token = jwt.sign(
+          {
+            email: employeeProfile.email,
+            employeeId: employeeProfile._id
+          },
+          process.env.JWT_KEY, 
+        {
+          expireIn: "2h"
+        }
+        );
+        await Token.findOneAndpdate({_employeeId: employeeProfile._id, type: "login"}, {token: token}, {new: true, upsert: true})
+        return res.status(200).json({
+          status: true,
+          message: "Logged in successfully.",
+          data: {
+            token,
+            employeeProfile
+          }
+        })
+      }
+      return res.status(401).json({
+        status: true, 
+        message: "Incorrect password, please try again.",
+        data: undefined
+      })
+    })
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: false,
+      message: "Server error, authentication failed.",
+      data: undefined
+    })
+  })
+})
+
+// router.post("/login", (req, res) => {
+//   // If this function gets called, authentication was successful.
+//   // `req.user` contains the authenticated user.
+//   let employeeProfileInfo = req.body.inputs;
+//   let user = await logIn(employeeProfileInfo);
+//   console.log("User login has succeeded!");
+//   console.log("Req.user is", req.user);
+//   res.sendStatus(200);
+//   res.send(user);
+// });
 
 router.get("/loggedInUser", function (req, res) {
   res.send(req.user);
 });
+
+router.get("/logout", async (req, res,) => {
+  Token.findOneAndDelete({_employeeId: req.employeeId, type: "login"}, (err, doc) => {
+    if(err) return res.status(401).json({
+      status: false,
+      message: "Server error, please try again.",
+    })
+    return res.status(200).json({
+      status: true,
+      message: "Logged out succesfully.",
+    })
+  })
+});
+
+// const strategy = new LocalStrategy()
 
 module.exports = router;

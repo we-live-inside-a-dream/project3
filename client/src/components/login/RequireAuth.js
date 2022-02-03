f45881fa7bfbb197bfc10534e6ded6a7fcaf5418
Brian Sauco <brian.sauco.95@gmail.com>
2022-02-02 22:31:31 -0700

    Created RequireAuth component.

---
+++ client/src/components/login/RequireAuth.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useContext } from "react";
+import AuthenticationContext from "./AuthenticationContext";
+import { Navigate } from "react-router-dom";
+
+function RequireAuth({ children }) {
+  let auth = useContext(AuthenticationContext);
+
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+export default RequireAuth;

eaa90c52ae8e541f1e91d5e017c735fea3ebe812
Brian Sauco <brian.sauco.95@gmail.com>
2022-02-02 23:22:11 -0700

    readded authContext

--- client/src/pages/common/HomeDashBoardPage.js
+++ client/src/pages/common/HomeDashBoardPage.js
@@ -3,11 +3,11 @@ import StyledPage from "../../components/reusable/styled-page/StyledPage";
 import StyledPageTitle from "../../components/reusable/styled-page/StyledPageTitle";
 import moment from "moment";
 import AuthenticationContext from "../../components/login/AuthenticationContext";
-import { Navigate } from "react-router-dom";
 
 function HomeDashBoardPage() {
   const [currentHour, setCurrentHour] = useState(moment().hour());
   const [hourGreeting, setHourGreeting] = useState("");
+  const authContext = useContext(AuthenticationContext);
 
   useEffect(() => {
     let getGreeting = function () {

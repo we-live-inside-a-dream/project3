e2881dd956341ec4c802104e75e6f9cd18f3e2bb
derek-birtwistle <derekbirtwistle@hotmail.com>
2022-02-12 21:10:55 -0700

    chat works again!

--- client/src/pages/common/MessangerPage2.js
+++ client/src/pages/common/MessangerPage2.js
@@ -37,10 +37,18 @@ export default function Messenger() {
   //socket.emit will take data from ("String",FETCH);
 
   useEffect(() => {
+    if (!user._id) return;
+    let id = user._id;
+    console.log("room id", id);
     const origin = window.location.origin;
     const host = origin.replace("http", "ws");
-    // console.log("window location...", window.location);
-    socket.current = io(host);
+    socket.current = io(host, { query: { id } });
+
+    // console.log(socket.current);
+  }, [user._id]);
+
+  useEffect(() => {
+    if (socket.current == null) return;
     socket.current.on("getMessage", (data) => {
       console.log("get message...", data);
       setArrivalMessage({
@@ -49,9 +57,10 @@ export default function Messenger() {
         createdAt: Date.now(),
       });
     });
-  }, []);
+  }, [socket]);
 
   useEffect(() => {
+    console.log("got the message", arrivalMessage);
     arrivalMessage &&
       currentChat?.members.includes(arrivalMessage.sender) &&
       setMessages((prev) => [...prev, arrivalMessage]);
@@ -59,7 +68,7 @@ export default function Messenger() {
 
   useEffect(() => {
     if (user._id) {
-      socket.current.emit("addUser", user._id);
+      // socket.current.emit("addUser", user._id);
       // socket.current.on("getUsers", (users) => {
       // setOnlineUsers(
       //   user.followings.filter((f) => users.some((u) => u.userId === f))
@@ -108,7 +117,7 @@ export default function Messenger() {
       }
     };
     getMessages();
-  }, [currentChat]);
+  }, [currentChat, arrivalMessage]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -117,23 +126,23 @@ export default function Messenger() {
       text: newMessage,
       conversationId: currentChat._id,
     };
-
-    // db.inventory.find({ tags: "red" });
-
     //member is an array of Id's for members of convo
-    const receiverId = currentChat.members.map((r) => r.value);
+    const recipients = currentChat.members.map((r) => r.value);
     console.log("socket...", socket.current);
     console.log("user id", user._id);
     console.log("currentChat", currentChat);
-    console.log("reveicerId", receiverId);
+    console.log("recipients", recipients);
+    // let newRecipients = recipients.slice(user._id);
+    // console.log("new Rec", newRecipients);
     socket.current.emit("sendMessage", {
-      senderId: user._id,
-      receiverId,
+      recipients,
+      sender: user._id,
       text: newMessage,
     });
 
     try {
       const res = await axios.post("api/messages", message);
+      // console.log("messages...", messages);
       setMessages([...messages, res.data]);
       setNewMessage("");
     } catch (err) {

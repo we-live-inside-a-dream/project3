4619ae1b4652917bb3e86ef93073dab078ccfe77
Brian Sauco <brian.sauco.95@gmail.com>
2022-02-24 15:35:04 -0700

    Form library draft

---
+++ client/src/components/login/FormLib.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useField } from "formik";
+import {
+  StyledTextInput,
+  StyledLabel,
+  StyledIcon,
+  ErrorMsg,
+} from "./LoginStyle";
+
+//eye icon for password
+import { FiEyeOff, FiEye } from "react-icons/fi";
+
+export const TextInput = ({ icon, ...props }) => {
+  const [field, meta] = useField(props);
+  const [show, setShow] = useState(false);
+  return (
+    <div style={{ position: "relative" }}>
+      <StyledLabel htmlFor={props.name}>{props.label}</StyledLabel>
+      {props.type !== "password" && <StyledTextInput {...field} {...props} />}
+
+      {props.type === "password" && (
+        <StyledTextInput
+          {...field}
+          {...props}
+          type={show ? "text" : "password"}
+        />
+      )}
+
+      <StyledIcon>{icon}</StyledIcon>
+      {props.type === "password" && (
+        <StyledIcon onClick={() => setShow(!show)} right>
+          {show && <FiEye />}
+          {!show && <FiEyeOff />}
+        </StyledIcon>
+      )}
+      {meta.touched && meta.error ? (
+        <ErrorMsg>{meta.error}</ErrorMsg>
+      ) : (
+        <ErrorMsg style={{ visibility: "hidden" }}>.</ErrorMsg>
+      )}
+    </div>
+  );
+};

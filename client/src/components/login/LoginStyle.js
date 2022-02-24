33140084bf7df1a9a61cbf457021929bfb735701
Brian Sauco <brian.sauco.95@gmail.com>
2022-02-24 15:32:46 -0700

    LoginStyle alternative

--- client/src/components/login/LoginStyle.js
+++ client/src/components/login/LoginStyle.js
@@ -44,6 +44,16 @@ export const StyledSubtitle = styled.p`
   margin-bottom: 25px;
 `;
 
+export const Avatar = styled.div`
+  width: 85px;
+  height: 85px;
+  border-radius: 50px;
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center;
+  margin: auto;
+`;
+
 export const StyledButton = styled(Link)`
   padding: 10px;
   width: 150px;
@@ -55,6 +65,7 @@ export const StyledButton = styled(Link)`
   text-decoration: none;
   text-align: center;
   transition: ease-in-out 0.3s;
+  outline: 0;
 
   &:hover {
     background-color: ${colors.primary};
@@ -68,4 +79,101 @@ export const ButtonGroup = styled.div`
   justify-content: space-around;
   flex-direction: row;
   margin-top: 25px;
+`;
+
+export const StyledTextInput = styled.input`
+  width: 280px;
+  padding: 15px;
+  padding-left: 55px;
+  font-size: 17px;
+  letter-spacing: 1px;
+  color: ${colors.light2};
+  background-color: ${colors.light2};
+  border: 0;
+  outline: 0;
+  display: block;
+  margin: 5px auto 10px auto;
+  transition: ease-in-out 0.3s;
+
+  ${(props) =>
+    props.onInvalid &&
+    `background-color: ${colors.red}; color: ${colors.primary};`}
+
+  &:focus {
+    background-color: ${colors.dark2};
+    color: ${colors.primary};
+  }
+`;
+
+export const StyledLabel = styled.p`
+  text-align: left;
+  font-size: 13px;
+  font-weight: bold;
+`;
+export const StyledFormArea = styled.div`
+  background-color: ${(props) => props.bg || colors.light1};
+  text-align: center;
+  padding: 45px 550px;
+`;
+export const StyledFormButton = styled.button`
+  padding: 10px;
+  width: 150px;
+  background-color: transparent;
+  font-size: 16px;
+  border: 2px solid ${colors.theme};
+  border-radius: 25px;
+  color: ${colors.theme};
+  transition: ease-in-out 0.2s;
+  outline: 0;
+
+  &:hover {
+    background-color: ${colors.primary};
+    font-weight: bold;
+    cursor: pointer;
+  }
+`;
+
+export const ErrorMsg = styled.div`
+  font-size: 11px;
+  color: ${colors.red};
+  margin-top: 5px;
+  margin-bottom: 10px;
+  text-align: left;
+`;
+export const ExtraText = styled.p`
+  font-size: ${(props) => props.size}px;
+  text-align: center;
+  color: ${(props) => (props.color ? props.color : colors.dark2)};
+  padding: 20px;
+  margin-top: 10px;
+`;
+
+export const TextLink = styled(Link)`
+  text-decoration: none;
+  color: ${colors.dark1};
+  transition: ease-in-out 0.3s;
+
+  &:hover {
+    text-decoration: underline;
+    letter-spacing: 2px;
+    font-weight: bold;
+  }
+`;
+
+//icons
+export const StyledIcon = styled.p`
+  color: ${colors.dark1};
+  position: absolute;
+  font-size: 21px;
+  top: 20px;
+  ${(props) => props.right && `right: 15px;`}
+  ${(props) => props.right && `left: 15px;`}
+`;
+
+//copyright
+export const CopyrightText = styled.p`
+  padding: 5px;
+  margin: 20px;
+  text-align: center;
+  color: ${colors.dark1};
 `;

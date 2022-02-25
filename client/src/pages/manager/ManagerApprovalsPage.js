8199d63977556f2cf9822c42f19c32cfb9e1cc14
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-02-25 15:27:00 -0700

    added buttons to change approval page tabs

--- client/src/pages/manager/ManagerApprovalsPage.js
+++ client/src/pages/manager/ManagerApprovalsPage.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TimeOffApproval from "../../components/employee-time-off/TimeOffApproval";
 import StyledPage from "../../components/reusable/styled-page/StyledPage";
 import StyledPageTitle from "../../components/reusable/styled-page/StyledPageTitle";
 import ManagerViewOfShiftSwapRequests from "../../components/schedules/shift-swapping/ManagerViewOfShiftSwapRequests";
+import StyledApprovalsButtonGroup from "../../components/employee-time-off/StyledApprovalsButtonGroup";
 
-const TimeOffApprovalPage = () => {
+const ApprovalPage = () => {
+  const [currentTab, setCurrentTab] = useState(1);
   return (
     <div>
       <StyledPage>
         <StyledPageTitle style={{ marginBottom: "40px" }}>
           APPROVALS PAGE
         </StyledPageTitle>
-        <TimeOffApproval />
-        <ManagerViewOfShiftSwapRequests />
+        <div>
+          <StyledApprovalsButtonGroup
+            currentTab={currentTab}
+            setCurrentTab={setCurrentTab}
+          />
+          {currentTab === 1 && <TimeOffApproval />}
+          {currentTab === 2 && <ManagerViewOfShiftSwapRequests />}
+        </div>
       </StyledPage>
     </div>
   );
 };
 
-export default TimeOffApprovalPage;
+export default ApprovalPage;

c2182c6e570f74f29ed7ac1ea964598bf4396dd4
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-02-26 18:05:41 -0700

    started creating business days form

---
+++ client/src/components/management-settings/BusinessDaysForm.js
@@ -0,0 +1,53 @@
+import React, { useState} from "react";
+
+
+
+const weekDaysData = [
+    {value: "monday", label: "Monday"},
+    {value: "tuesday", label: "Tuesday"},
+    {value: "wednesday", label: "Wednesday"},
+    {value: "thursday", label: "Thursday"},
+    {value: "friday", label: "Friday"},
+    {value: "saturday", label: "Saturday"},
+    {value: "sunday", label: "Sunday"}
+]
+function BusinessDaysSettings() {
+    const [createWeekDays, setCreateWeekDays] = useState()
+
+    const createWeekDayHandler = (newWeekDay) => {
+        setCreateWeekDays(newWeekDay);
+        console.log("created weekday", newWeekDay);
+      };
+
+    async function crateBusinessDays(newBusinessDays) {
+        await fetch("/api/businessDays", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newBusinessDays)
+        })
+        setCreateWeekDays(newBusinessDays)
+    }
+
+    async function postData() {
+        let newBusinessDays = {
+            createWeekDays: newBusinessDays.value
+        }
+    }
+    
+    return (
+        <div>
+            <input>Week Day</input>
+            <input>Open Time</input>
+            <input>Close Time</input>
+            <Select 
+            defaultValue={createWeekDays}
+            options={weekDaysData}
+            onChange={createWeekDayHandler}
+            />
+        </div>
+    )
+}
+
+export default BusinessDaysSettings

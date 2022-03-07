958059ce2c2b046f152418cc2deeca38c3355609
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-03-07 12:17:24 -0700

    fixed some bugs

--- client/src/components/reusable/Inputs/BasicTimePicker.js
+++ client/src/components/reusable/Inputs/BasicTimePicker.js
@@ -6,7 +6,8 @@ import styled from "styled-components";
 import parseISO from "date-fns/parseISO";
 
 const StyledBasicTimePicker = styled.div`
-  padding: 5px;
+  /* padding: 5px; */
+  margin-left: 0px;
 `;
 // dateAdapter={AdapterDateFns({lib:"date-fns"},{formats:"fullTime24h"})}>
 export default function BasicTimePicker({ onChange, value, label }) {

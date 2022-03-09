9356c5b5a4e30b249dcbd73b373fdc07c5884c76
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-03-08 23:00:48 -0700

    made approvals navbox render

--- client/src/components/dashboard/ApprovalsBox.js
+++ client/src/components/dashboard/ApprovalsBox.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DashboardBoxScale from "./DashboardBoxScale";
 // import EmployeeUpcomingShiftList from "./EmployeeUpcomingShiftList";
 
+function ScheduleContent() {
+  const [swapsPending, setSwapsPending] = useState(false);
+  const [timeOffPending, setTimeOffPending] = useState(false);
+
+  useEffect(() => {
+    async function findShiftSwapRequests() {
+      let fetchedResult = await fetch(
+        "/api/schedule/find-pending-swap-requests"
+      );
+      let retreivedSwapRequests = await fetchedResult.json();
+      if (retreivedSwapRequests?.length > 0) {
+        setSwapsPending(true);
+      } else setSwapsPending(false);
+    }
+    findShiftSwapRequests();
+  }, []);
+
+  useEffect(() => {
+    async function fetchTimeOff() {
+      let fetchResult = await fetch("/api/timeOff/list");
+      let fetchedTimeOff = await fetchResult.json();
+      if (fetchedTimeOff?.length > 0) {
+        setTimeOffPending(true);
+      } else setTimeOffPending(false);
+    }
+    fetchTimeOff();
+  }, []);
+
+  return (
+    <div>
+      <h3 style={{ color: "var(--accentColorTitle)", margin: "0px auto" }}>
+        SHIFT SWAP REQUESTS: {swapsPending === true ? "⏳" : "✅"}
+      </h3>
+      {swapsPending === true ? (
+        <p>you have pending shift swap requests </p>
+      ) : (
+        <p>you are up to date with shift swap requests </p>
+      )}
+      <h3 style={{ color: "var(--accentColorTitle)", margin: "0px auto" }}>
+        TIME OFF REQUESTS: {timeOffPending === true ? "⏳" : "✅"}
+      </h3>
+      {timeOffPending === true ? (
+        <p>you have pending time off requests </p>
+      ) : (
+        <p>you are up to date with time off requests </p>
+      )}
+    </div>
+  );
+}
 function ScheduleBox() {
   let navigate = useNavigate();
-  let content = "THERE ARE SOME PENDING APPROVALS";
 
   return (
     <>
@@ -17,7 +65,7 @@ function ScheduleBox() {
         transformOrigin={"top left"}
         title="MANAGER APPROVALS"
         clickFunction={() => navigate("/managerApprovalsPage")}
-        content={content}
+        content={<ScheduleContent />}
       />
     </>
   );

7aec03ecb73b30802b5ef259284ccf34f2442ecd
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-03-09 10:06:49 -0700

    added scroll back onto styled  page

--- client/src/components/reusable/styled-page/StyledPage.js
+++ client/src/components/reusable/styled-page/StyledPage.js
@@ -5,9 +5,9 @@ const StyledPage = styled.div`
   padding: 20px;
   width: 80%;
   margin: auto;
-  /* overflow-y: auto; */
+  overflow-y: auto;
   height: 86vh;
-  /* filter: drop-shadow(3px 3px 10px lightgrey); */
+  filter: drop-shadow(3px 3px 10px lightgrey);
 `;
 
 export default StyledPage;
